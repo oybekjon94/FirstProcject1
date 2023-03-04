3b198ea2ca016e60910df2628f83cbb0edc14ed2
oybekjon94 <x.oybek94@gmail.com>
2023-03-04 12:04:17 +0900

    Initial commit

---
+++ 02-options-menu/src/main/java/uz/oybekdev/a02optionsmenu/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.oybekdev.a02optionsmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
